5358f8915d39074eb26355615c90105d65c988e4
git-dmitriy <shalberkind@mail.ru>
2022-03-21 23:33:18 +0300

    Add session switcher

--- src/components/timer/Controls.tsx
+++ src/components/timer/Controls.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import styled from 'styled-components';
-import { MdPlayArrow, MdPause } from 'react-icons/md';
-import { GrPowerReset } from 'react-icons/gr';
-
+import { useLocalStorage } from 'components/hooks/useLocalsotrage';
+import { Config } from 'features/timer/types';
 import { useAppSelector, useAppDispatch } from 'app/hooks';
-import { init, start, tick, pause } from 'features/timer/timerSlice';
+import {
+  init,
+  start,
+  tick,
+  pause,
+  nextSession,
+  cycleComplete,
+} from 'features/timer/timerSlice';
+import { MdPlayArrow, MdPause, MdSkipNext } from 'react-icons/md';
+import { GrPowerReset } from 'react-icons/gr';
 
 const ControlBtns = styled.div`
   display: flex;
@@ -11,11 +20,23 @@ const ControlBtns = styled.div`
 `;
 
 export const Controls = () => {
-  const { time, currentSession, isRunning, timerId } = useAppSelector(
+  const { time, sessionNumber, config, isRunning, timerId } = useAppSelector(
     (state) => state.timer
   );
-
   const dispatch = useAppDispatch();
+  const [localConfig] = useLocalStorage<Config>('config', config);
+
+  useEffect(() => {
+    if (time === 0 && isRunning) {
+      switchToNextSession();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [time, isRunning]);
+
+  useEffect(() => {
+    dispatch(init(localConfig));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const startHandler = () => {
     const timer = setInterval(() => {
@@ -31,7 +52,25 @@ export const Controls = () => {
 
   const resetHandler = () => {
     timerId && clearInterval(timerId);
-    dispatch(init(null));
+    dispatch(init(localConfig));
+  };
+
+  const switchToNextSession = () => {
+    if (sessionNumber % 2 === 0) {
+      if (sessionNumber === config.sessions.length - 2) {
+        dispatch(nextSession('rest'));
+      } else {
+        dispatch(nextSession('break'));
+      }
+    } else if (sessionNumber % 2 !== 0) {
+      if (sessionNumber === config.sessions.length - 1) {
+        timerId && clearInterval(timerId);
+        dispatch(cycleComplete());
+        dispatch(init(localConfig));
+      } else {
+        dispatch(nextSession('focus'));
+      }
+    }
   };
 
   return (
@@ -45,6 +84,9 @@ export const Controls = () => {
           <MdPlayArrow />
         </button>
       )}
+      <button onClick={switchToNextSession}>
+        <MdSkipNext />
+      </button>
       <button onClick={resetHandler}>
         <GrPowerReset />
       </button>
