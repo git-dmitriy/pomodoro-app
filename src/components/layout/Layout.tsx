6f73e49d01417f1c7606d1dce3ab512d9394dca1
git-dmitriy <shalberkind@mail.ru>
2022-03-25 23:46:57 +0300

    Create dynamic background fill

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,38 @@
+import { useAppSelector } from 'app/hooks';
 import styled from 'styled-components';
 
-const StyledLayout = styled.div`
+type P = {
+  bg: 'focus' | 'break' | 'rest' | 'standby';
+};
+
+const StyledLayout = styled.div<P>`
   width: 100%;
   height: 100vh;
-  background-color: #f3f4f6;
-  color: #4b5563;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  transition: background-color 0.3s;
+  background-color: ${(props) => handleColorType(props.bg)};
 `;
 
+const handleColorType = (color: string) => {
+  switch (color) {
+    case 'focus':
+      return '#bc4b51';
+    case 'break':
+      return '#5b8e7d';
+    case 'rest':
+      return '#8cb369';
+    default:
+      return '#22333b';
+  }
+};
+
 export const Layout: React.FC = ({ children }) => {
-  return <StyledLayout>{children}</StyledLayout>;
+  const { isRunning, currentSession } = useAppSelector((state) => state.timer);
+
+  const backgroundColor = isRunning ? currentSession : 'standby';
+
+  return <StyledLayout bg={backgroundColor}>{children}</StyledLayout>;
 };
