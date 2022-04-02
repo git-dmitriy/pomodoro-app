16090b4470fb75abd16794ec285e34fbe4da8a51
git-dmitriy <shalberkind@mail.ru>
2022-04-02 23:59:28 +0300

    Fix bug with keys

--- src/components/tasks/tasksList.tsx
+++ src/components/tasks/tasksList.tsx
@@ -3,56 +3,55 @@ import { AddTaskForm } from 'components/tasks/AddTaskForm';
 import { TaskItem } from 'features/tasks/types';
 import { TaskListItem } from 'components/tasks/TaskListItem';
 import { useDispatch } from 'react-redux';
-import { removeCompleteTasks } from 'features/tasks/tasksSlice';
+import { removeCompletedTasks } from 'features/tasks/tasksSlice';
+import { MdDeleteSweep } from 'react-icons/md';
+import styled from 'styled-components';
+import { Button } from 'components/ui/Button';
+import { useAppSelector } from 'app/hooks';
 
 type Tasks = {
   tasks: TaskItem[];
 };
 
+const Container = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 0;
+`;
+
 export const TasksList: React.FC = () => {
   const dispatch = useDispatch();
-
-  const tasks = useSelector((state: Tasks) => state.tasks);
+  const tasks = useAppSelector((state: Tasks) => state.tasks);
   const completed = tasks.filter((task) => task.isComplete === true);
-
-  const removeCompleteTasksHandler = () => dispatch(removeCompleteTasks());
+  const removeCompleteTasksHandler = () => dispatch(removeCompletedTasks());
 
   return (
-    <>
+    <Section>
       <AddTaskForm />
-      <b>Не выполненные</b>
-      <ul>
-        pupm-purum
-        {tasks.map((item, idx) => {
-          if (item.isComplete === false) {
-            return (
-              <li key={idx}>
-                <TaskListItem {...item} />
-              </li>
-            );
-          }
-          return null;
-        })}
-      </ul>
-      <b>Выполненные</b>
-      <ul>
-        {tasks.map((item, idx) => {
-          if (item.isComplete === true) {
-            return (
-              <li key={idx}>
-                <TaskListItem {...item} />
-              </li>
-            );
-          }
-          return null;
-        })}
-      </ul>
-
       {completed.length !== 0 ? (
-        <button onClick={removeCompleteTasksHandler}>
-          Очистить выполненные задачи
-        </button>
-      ) : null}
-    </>
+        <Button onClick={removeCompleteTasksHandler}>
+          <MdDeleteSweep />
+        </Button>
+      ) : (
+        <Button disabled>
+          <MdDeleteSweep />
+        </Button>
+      )}
+      <Container>
+        <ul>
+          {tasks.map((item) => (
+            <li key={item.id}>
+              <TaskListItem {...item} />
+            </li>
+          ))}
+        </ul>
+      </Container>
+    </Section>
   );
 };
