f1aed66599f360b8ec0d17e65dc581bdd56a8a70
git-dmitriy <shalberkind@mail.ru>
2022-04-03 23:57:56 +0300

    Create custom input field

---
+++ src/components/ui/InputNumber.tsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  width: 100px;
+  color: inherit;
+  border: none;
+  background-color: transparent;
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  text-align: center;
+  font-size: 30px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 2px;
+  border-radius: 20px;
+  padding: 5px;
+`;
+
+type P = {
+  id: string;
+  name: string;
+  min: number;
+  max: number;
+  step: number;
+  value: number;
+  label: string;
+  required?: boolean;
+  onChangeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const InputNumber = ({
+  id,
+  name,
+  min,
+  max,
+  step,
+  value,
+  label,
+  required,
+  onChangeHandler,
+}: P) => {
+  return (
+    <Container>
+      <label htmlFor={id}>{label}</label>
+      <Input
+        id={id}
+        name={name}
+        min={min}
+        max={max}
+        step={step}
+        type='number'
+        value={value}
+        onChange={onChangeHandler}
+      />
+    </Container>
+  );
+};
