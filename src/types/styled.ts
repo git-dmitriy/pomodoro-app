9cb57982eefc5396a42c3192d0baa7ec66eac896
git-dmitriy <dmitriy.shalberkin@gmail.com>
2025-05-27 15:08:53 +0300

    refactor: fix styled types

--- src/types/styled.ts
+++ src/types/styled.ts
@@ -1,17 +1,20 @@
-export interface ITheme {
-    colors: {
-        focus: string;
-        break: string;
-        rest: string;
-        standby: string;
-    };
+import 'styled-components';
 
-    media: {
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        xl: string;
-        '2xl': string;
-    };
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        colors: {
+            focus: string;
+            break: string;
+            rest: string;
+            standby: string;
+        };
+        media: {
+            xs: string;
+            sm: string;
+            md: string;
+            lg: string;
+            xl: string;
+            '2xl': string;
+        };
+    }
 }
