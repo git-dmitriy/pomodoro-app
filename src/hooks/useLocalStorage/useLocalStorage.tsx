cf0bfaf929bd34722864964566db9d5051f9e0ee
git-dmitriy <dmitriy.shalberkin@gmail.com>
2025-09-01 23:34:34 +0300

    refactor: add utility to manage local storage

--- src/hooks/useLocalStorage/useLocalStorage.tsx
+++ src/hooks/useLocalStorage/useLocalStorage.tsx
@@ -1,30 +1,33 @@
-import { useState } from 'react';
+import {Dispatch, SetStateAction, useState} from 'react';
+import {localStorageUtil} from '@/utils/localStorageUtil';
 
-export function useLocalStorage<T>(key: string, initialValue: T) {
+export function useLocalStorage<T>(key: string, initialValue: T): [T, Dispatch<SetStateAction<T>>] {
     const [storedValue, setStoredValue] = useState<T>(() => {
         if (typeof window === 'undefined') {
             return initialValue;
         }
         try {
-            const item = window.localStorage.getItem(key);
-            return item ? JSON.parse(item) : initialValue;
+            const item = localStorageUtil.getItem<T>(key, initialValue);
+            return item ?? initialValue;
         } catch (error) {
             console.error(error);
             return initialValue;
         }
     });
 
-    const setValue = (value: T | ((val: T) => T)) => {
+    const setValue: Dispatch<SetStateAction<T>> = (value: T | ((val: T) => T)) => {
         try {
             const valueToStore =
                 value instanceof Function ? value(storedValue) : value;
+
             setStoredValue(valueToStore);
+
             if (typeof window !== 'undefined') {
-                window.localStorage.setItem(key, JSON.stringify(valueToStore));
+                localStorageUtil.setItem<T>(key, valueToStore);
             }
         } catch (error) {
             console.error(error);
         }
     };
-    return [storedValue, setValue] as const;
+    return [storedValue, setValue];
 }
