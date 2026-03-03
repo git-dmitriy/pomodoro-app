63b83045843326433d6e4acbde2f385bca56f4b0
git-dmitriy <shalberkind@mail.ru>
2026-03-03 17:47:40 +0300

    feat(app: PWA support

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App'
+
+if ('serviceWorker' in navigator) {
+  registerSW({ immediate: true })
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
